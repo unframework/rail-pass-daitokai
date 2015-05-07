06688fab93cd04472817619d9d9a35d0b0cfd42e
Nick Matantsev <nick.matantsev@gmail.com>
2015-05-06 21:47:04 -0400

    Removed reliance on jQuery in input lib

--- src/Input.js
+++ src/Input.js
@@ -9,7 +9,7 @@ function Input(whichCodeMap) {
         gameCodeStatus[whichCodeMap[num]] = false;
     }
 
-    $(document.body).keydown(function (e) {
+    document.body.onkeydown = function (e) {
         var gameCode = whichCodeMap[e.which];
 
         if (gameCode === undefined) {
@@ -23,11 +23,9 @@ function Input(whichCodeMap) {
         }
 
         gameCodeStatus[gameCode] = true;
+    };
 
-        $(self).trigger('key:' + gameCode, [ true ]);
-    });
-
-    $(document.body).keyup(function (e) {
+    document.body.onkeyup = function (e) {
         var gameCode = whichCodeMap[e.which];
 
         if (gameCode === undefined) {
@@ -41,19 +39,15 @@ function Input(whichCodeMap) {
         }
 
         gameCodeStatus[gameCode] = false;
+    };
 
-        $(self).trigger('key:' + gameCode, [ false ]);
-    });
-
-    $(window).blur(function (e) {
+    window.onblur = function (e) {
         for (var gameCode in gameCodeStatus) {
             if (gameCodeStatus[gameCode]) {
                 gameCodeStatus[gameCode] = false;
-
-                $(self).trigger('key:' + gameCode, [ false ]);
             }
         }
-    });
+    };
 }
 
 module.exports = Input;
